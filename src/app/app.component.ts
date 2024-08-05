237a5cd4012110f13b340f8a955d528c722dce20
Gabriel Rodrigues <rodriguesgasoares@gmail.com>
2024-08-04 22:45:06 -0300

    feat: add  loading animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { TodoComponent } from "./components/todo/todo.component";
 import { BackTopComponent } from "./components/back-top/back-top.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TodoComponent, BackTopComponent],
+  imports: [ CommonModule, HeaderComponent, TodoComponent, BackTopComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+  isLoading = true;
+
+  ngOnInit(): void {
+    window.addEventListener('load', () => {
+      this.isLoading = false;
+    })
+  }
 }
