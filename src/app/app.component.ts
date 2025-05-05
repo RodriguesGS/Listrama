a7fc0d9001a417179c2cf4c4ea63bd87799a44c7
Gabriel <rodriguesgasoares@gmail.com>
2025-05-05 15:37:44 -0300

    feat: add esqueleto inicial da lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputComponent } from "./components/input/input.component";
+import { TasksComponent } from "./components/tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [ MatIconModule ],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss'
+})
+export class InputComponent {
+
+}
