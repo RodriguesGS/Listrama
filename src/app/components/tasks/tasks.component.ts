19c4ad9e0278ce44f3f8d05eede05c564246c0be
Gabriel <rodriguesgasoares@gmail.com>
2025-05-06 15:28:01 -0300

    feat: add delete function

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [ MatIconModule ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
 export class TasksComponent {
   @Input() tasks: string[] = [];
 
+  deleteTask(i: number) {
+    this.tasks.splice(i, 1)
+  }
 }
