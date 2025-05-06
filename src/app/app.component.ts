3084f77e99244b39eee0e7f645ce684974c54ccd
Gabriel <rodriguesgasoares@gmail.com>
2025-05-06 10:06:30 -0300

    feat: add create tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { InputComponent } from "./components/input/input.component";
 import { TasksComponent } from "./components/tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InputComponent, TasksComponent],
+  imports: [
+    InputComponent, 
+    TasksComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'listrama';
+  tasks: string[] = [];
+
+  onTaskCreated(newTask: string) {
+    this.tasks.push(newTask);
+  }
 }
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [ MatIconModule ],
+  imports: [
+    MatIconModule,
+    FormsModule
+  ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
 export class InputComponent {
+  @Output() task = new EventEmitter<string>();
+  taskText: string = '';
 
+  addTask() {
+    if (this.taskText.trim()) {
+      this.task.emit(this.taskText.trim());
+      this.taskText = '';
+    }
+  }
 }
